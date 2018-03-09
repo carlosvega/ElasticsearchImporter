a7cbf2783438ac0baf63abf957c7ec061a0a183b
Carlos Vega <carlos.vega@naudit.es>
2018-03-09 18:15:50 +0100

    changed installer

--- install.bash
+++ install.bash
@@ -13,13 +13,6 @@ if command -v apt-get &> /dev/null ; then
 	sudo apt-get remove -y sqlite3
 	sudo apt-get purge -y sqlite3
 	sudo apt-get install -y build-essential bzip2 git libbz2-dev libc6-dev libgdbm-dev libgeos-dev liblz-dev liblzma-dev libncurses5-dev libncursesw5-dev libreadline6 libreadline6-dev libsqlite3-dev libssl-dev lzma-dev python-dev python-pip python-software-properties python-virtualenv software-properties-common sqlite3 tcl tk-dev tk8.5-dev wget
-	#install pypy
-	echo "[ - Installing pypy... ]"
-	sudo add-apt-repository -y ppa:pypy/ppa
-	sudo apt-get update -y
-	sudo apt-get install -y pypy pypy-dev
-	sudo apt-get install -y --only-upgrade pypy 
-	sudo pip install pip --upgrade
 	#install sqlite
 	echo "[ - Installing sqlite3... ]"
 	sudo wget "https://www.sqlite.org/src/tarball/sqlite.tar.gz?r=release" -O sqlite.tar.gz &> /dev/null
@@ -31,6 +24,14 @@ if command -v apt-get &> /dev/null ; then
 	cd ..
 	sudo apt-get remove -y python python3 python-dev
 	sudo apt-get install -y --reinstall python2.7 python3 python-dev
+	sudo apt-get install -y build-essential bzip2 git libbz2-dev libc6-dev libgdbm-dev libgeos-dev liblz-dev liblzma-dev libncurses5-dev libncursesw5-dev libreadline6 libreadline6-dev libsqlite3-dev libssl-dev lzma-dev python-dev python-pip python-software-properties python-virtualenv software-properties-common sqlite3 tcl tk-dev tk8.5-dev wget
+	#install pypy
+	echo "[ - Installing pypy... ]"
+	sudo add-apt-repository -y ppa:pypy/ppa
+	sudo apt-get update -y
+	sudo apt-get install -y pypy pypy-dev
+	sudo apt-get install -y --only-upgrade pypy 
+	sudo pip install pip --upgrade
 	#install repo
 	#echo "[ - Installing ElasticsearchImporter repository... ]"
 	#sudo git clone https://github.com/carlosvega/ElasticsearchImporter.git ElasticsearchImporter
