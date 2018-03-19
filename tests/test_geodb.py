916e60d4952468df48b98d09d15331b09b6be6f8
Carlos Vega <carlosgonzalo.vega@estudiante.uam.es>
2018-03-19 17:22:57 +0100

    fixed test

--- tests/test_geodb.py
+++ tests/test_geodb.py
@@ -32,7 +32,7 @@ class Test_geodb(object):
 	# 	assert ZIPLevel_GeoDB.check_FTS5_support() == True, 'FTS5 extension not available in your sqlite3 installation. py-sqlite3 version: {}. sqlite3 version: {}. Please, recompile or reinstall sqlite3 modules with FTS5 support.'.format(sqlite3.version, sqlite3.sqlite_version)
 
 
-	IP LEVEL
+	#IP LEVEL
 	def test_ip_level_db(self, ip_level_db):
 		result = ip_level_db.get_geodata('ip', '8.8.8.8')
 		check_val = {'country_code': 'US', 'country_name': 'UNITED STATES', 'location': '37.405992,-122.078515', 'place_name': 'MOUNTAIN VIEW', 'region_name': 'CALIFORNIA', 'representative_point': '37.405992,-122.078515', 'zip_code': '94043'}
