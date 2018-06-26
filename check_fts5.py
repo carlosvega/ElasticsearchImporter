23d2ff32455a0b189f1467ebf6b709156e539071
Carlos Vega <carlosgonzalo.vega@estudiante.uam.es>
2018-06-26 17:57:22 +0200

    added script to check if there is FTS5 support

---
+++ check_fts5.py
@@ -0,0 +1,16 @@
+import sqlite3, sys
+
+con = sqlite3.connect(':memory:')
+cur = con.cursor()
+cur.execute('pragma compile_options;')
+available_pragmas = cur.fetchall()
+con.close()
+
+print(available_pragmas)
+
+if ('ENABLE_FTS5',) in available_pragmas:
+    print('YES')
+    sys.exit(1)
+else:
+    print('NO')
+    sys.exit(0)
