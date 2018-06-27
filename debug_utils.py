3300196d94ea34e3d8bc69a5c8e5507443e1822e
Carlos Vega <carlosgonzalo.vega@estudiante.uam.es>
2018-06-27 11:31:08 +0200

    added peak memory usage

--- debug_utils.py
+++ debug_utils.py
@@ -1,6 +1,12 @@
-import os, psutil, logging
+import os, psutil, logging, platform, resource
 
 def log_rss_memory_usage(msg=''):
+	max_rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+	if platform == 'Darwin':
+		max_rss = max_rss/1e3
+	else:
+		max_rss = max_rss/1e6
+
 	rss = psutil.Process(os.getpid()).memory_info().rss
 	thislog = logging.getLogger(__name__)
-	thislog.info('Memory usage: {:.2f} MB. {}'.format(rss/1e6, msg))
+	thislog.info('Memory usage: {:.2f} MB. (Peak so far: {:.2f} MB) {}'.format(rss/1e6, max_rss, msg))
