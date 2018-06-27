921534ef9c1638a2354803a9ea3dbba414386f12
Carlos Vega <carlosgonzalo.vega@estudiante.uam.es>
2018-06-27 11:54:06 +0200

    added functions for memory usage

--- debug_utils.py
+++ debug_utils.py
@@ -10,3 +10,9 @@ def log_rss_memory_usage(msg=''):
 	rss = psutil.Process(os.getpid()).memory_info().rss
 	thislog = logging.getLogger(__name__)
 	thislog.info('Memory usage: {:.2f} MB. (Peak so far: {:.2f} MB) {}'.format(rss/1e6, max_rss, msg))
+
+def get_memory_status():
+	return psutil.virtual_memory()
+
+def get_available_memory():
+	return psutil.virtual_memory().available/1e6
