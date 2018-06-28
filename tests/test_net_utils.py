b7e19063f179c8fe6fa62120cb43ccbef131e65b
Carlos Vega <carlosgonzalo.vega@estudiante.uam.es>
2018-06-28 18:59:56 +0200

    added tests for unicode ips

--- tests/test_net_utils.py
+++ tests/test_net_utils.py
@@ -8,11 +8,13 @@ from net_utils import *
 class Test_netutils(object):
 	def test_int2ip(self, sample_ips):
 		for ip, int_ip, mask in sample_ips:
-			assert int2ip(int_ip) == ip 
+			assert int2ip(int_ip) == ip
 
 	def test_ip2int(self, sample_ips):
 		for ip, int_ip, mask in sample_ips:
 			assert ip2int(ip) == int_ip
+		for ip, int_ip, mask in sample_ips:
+			assert ip2int(unicode(ip)) == int_ip
 
 	def test_in_net(self, sample_ips, bad_sample_ips):
 		for ip, int_ip, mask in sample_ips:
