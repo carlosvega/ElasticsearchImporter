ea19d6f3b856b664b16c2bf669edc6347c04d082
Carlos Vega <carlosgonzalo.vega@estudiante.uam.es>
2018-07-13 11:22:15 +0200

    added timeit decorator and get_script_path

--- debug_utils.py
+++ debug_utils.py
@@ -1,4 +1,4 @@
-import os, psutil, logging, platform, resource
+import os, psutil, logging, platform, resource, time, sys
 
 def log_rss_memory_usage(msg=''):
 	max_rss = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
@@ -16,3 +16,20 @@ def get_memory_status():
 
 def get_available_memory():
 	return psutil.virtual_memory().available/1e6
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
+
+def get_script_path():
+	return os.path.dirname(os.path.realpath(sys.argv[0]))
